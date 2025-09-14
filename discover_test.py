4429e183054d1ac64a6a03f0b1344aa9bbb2e7ab
Alvin T. Veroy <me@alvin.tech>
2025-09-14 20:28:40 +0800

    Delete discover_test.py (#2)

--- discover_test.py
+++
@@ -1,8 +0,0 @@
-import pychromecast
-
-chromecasts, browser = pychromecast.get_listed_chromecasts()
-print(f"Found {len(chromecasts)} devices")
-for cast in chromecasts:
-    print(cast)
-
-browser.stop_discovery()
